61f3dd50333c212293b6ff7afe078f8ba95c3bac
Iskander Alimov <rocketikman@yandex.ru>
2024-10-11 21:15:46 +0300

    Server running on port

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	//Get .env variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file, using default environment variables")
+	}
+
+	port := getPort()
+
+	//Static files
+	http.Handle("/", http.FileServer(http.Dir("./web")))
+
+	//Server initialization
+	fmt.Printf("Server running on port %s...", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+
+}
+
+func getPort() string {
+	port := os.Getenv("TODO_PORT")
+	if port == "" {
+		port = "7540"
+	}
+	return port
+}
